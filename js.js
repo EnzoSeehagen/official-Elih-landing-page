689f9435fd7bc67dcd8e83d355b70e9bc006ead6
Enzo Seehagen <enzovonseehagen@gmail.com>
2023-08-06 00:20:49 -0300

    avançando mais um pouco

--- js.js
+++ js.js
@@ -18,3 +18,30 @@ function toggleContact(){
 
 }
 // End Toggle Contact form
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 2,
+          spaceBetween: 50,
+        },
+      }
+  });
